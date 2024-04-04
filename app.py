1c14aa00010e432f1171b2ce81825b6b5caba169
Artem Suchkov <artem.suchkov@technologiivokrug.ru>
2024-04-04 20:02:52 +0300

    fix env

--- app.py
+++ app.py
@@ -48,11 +48,11 @@ def get_weather() -> pd.DataFrame :
 
 def send_message(message):
 
-    TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
     TELEGRAM_CHANNEL_ID = os.getenv('TELEGRAM_CHANNEL_ID')
 
     url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
-        
+
     params = {
         'chat_id': TELEGRAM_CHANNEL_ID,
         'text': f'{message}'
